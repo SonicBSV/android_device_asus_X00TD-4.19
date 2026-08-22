d38fa3edb7687309bebc7ab950c6652537ca26d3
SonicBSV <sonicbsv@ya.ru>
2026-08-22 11:52:12 +0300

    X00TD: update lineage_X00TD.mk

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -7,13 +7,18 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common crDroid stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_DISABLE_MATLOG := true
+
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_X00TD
@@ -25,5 +30,12 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=ASUS_X00TD \
-    PRODUCT_NAME=WW_X00TD
+    TARGET_DEVICE=ASUS_X00T_2 \
+    PRODUCT_NAME=WW_X00TD \
+    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 17.2017.2012.438-20210129 release-keys"
+
+# Оригинальный сертифицированный отпечаток ASUS (Android 10 - финальное обновление)
+BUILD_FINGERPRINT := asus/WW_X00TD/ASUS_X00T_2:10/QKQ1/17.2017.2012.438-20210129:user/release-keys
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
