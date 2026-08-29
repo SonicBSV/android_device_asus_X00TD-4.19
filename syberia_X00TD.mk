3e7bc05e22d2649a4275b564b80b83e255844600
SonicBSV <sonicbsv@ya.ru>
2026-08-29 09:16:23 +0300

    X00TD: adapth for Syberia OS AOSP/CAF build

--- syberia_X00TD.mk
+++ syberia_X00TD.mk
@@ -8,20 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common crDroid stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common syberia stuff
+$(call inherit-product, vendor/syberia/config/common_full_phone.mk)
+$(call inherit-product, vendor/syberia/common.mk)
+
+WITH_GMS := true
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-TARGET_EXCLUDES_AUDIOFX := true
-TARGET_DISABLE_MATLOG := true
+# Bootanimation (X00TD имеет разрешение экрана 1080x2160)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # FaceUnlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := syberia_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -29,6 +32,7 @@ PRODUCT_MANUFACTURER := asus
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
+# Исправленный блок переопределений для убирания ошибки «Внутренняя ошибка»
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=ASUS_X00T_2 \
     PRODUCT_NAME=WW_X00TD \
